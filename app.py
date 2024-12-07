4d19f4252586b4e9ff87e339589503a831ccd390
Jeffrey Goett <jgoett2@yahoo.com>
2024-12-07 09:32:07 -0500

    fix dataframe

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ st.title("My Test App")
 
 st.text_area(label="Your Question Here")
 
-df = pd.DataFrame({"name":"Jeff", "age":1},
-                  {"name":"Dave", "age":2})
+df = pd.DataFrame([{"name":"Jeff", "age":1},
+                  {"name":"Dave", "age":2}])
 
+print(df)
 st.table(data=df)
