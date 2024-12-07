ea4da3e3b3f0672720fb1c37b59351c034a01546
Jeffrey Goett <jgoett2@yahoo.com>
2024-12-07 14:27:03 -0500

    update app

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ st.text_area(label="Your Question Here")
 df = pd.DataFrame([{"name":"Jeff", "age":1},
                   {"name":"Dave", "age":2}])
 
-print(df)
 st.table(data=df)
 
-st.button("Hello!")
+def call_back():
+    df[df.Name=="Jeff", "age"] += 1
+
+st.button("Hello!", on_click=call_back)
