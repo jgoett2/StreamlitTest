56bbdada7df0c951c408ed1f9737645f1b2ead95
Jeffrey Goett <jgoett2@yahoo.com>
2024-12-07 09:21:32 -0500

    Update

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("My Test App")
+
+st.text_area(label="Your Question Here")
+
+df = pd.DataFrame({"name":"Jeff", "age":1},
+                  {"name":"Dave", "age":2})
+
+st.table(data=df)
