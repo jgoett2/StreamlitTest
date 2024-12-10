210cab76bffece2fd3ce20458718145c73cd877e
Jeffrey Goett <jgoett2@yahoo.com>
2024-12-10 12:47:19 -0500

    update app

--- app.py
+++ app.py
@@ -4,13 +4,23 @@ import numpy as np
 
 st.title("CFP")
 
+
 df = pd.read_csv("data.csv")
 
-key = pd.Series({"CFP_First_Round1": "2. Notre Dame"})
+if "key" not in st.session_state:
+  st.session_state.key = pd.Series({})
 
-df["Wins"] = np.sum(df.eq(key), axis=1)
-df["Losses"] = len(key) - df["Wins"] 
+df["Wins"] = np.sum(df.eq(st.session_state.key), axis=1)
+df["Losses"] = len(st.session_state.key) - df["Wins"] 
 df = df.sort_values("Wins", ascending=False)
 
 st.dataframe(df[["Name", "Wins", "Losses"]], hide_index=True)
+
+def callback_function():
+    #print(st.session_state.Game1)
+    st.session_state.key["CFP_FirstRound1"] = st.session_state.CFP1
+    pass
+
+st.radio("CFP First Round", ["1. Indiana", "2. Notre Dame", "open"], key="CFP1", index=2, horizontal=True, on_change=callback_function)
+
 
