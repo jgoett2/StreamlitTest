ecf8760ed205e4d7aba328d03dc61507ffaccc68
Jeffrey Goett <jgoett2@yahoo.com>
2024-12-10 07:03:55 -0500

    Updated App

--- app.py
+++ app.py
@@ -2,26 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("My Test App")
+st.title("CFP")
 
-if "key" not in st.session_state:
-  st.session_state.key = {"Game1":"Notre Dame", "Game2":"Penn State"}
+df = pd.read_csv("data.csv")
 
-df = pd.DataFrame([{"Name":"Jeff", "Game1":"Notre Dame", "Game2":"Penn State"},
-             {"Name":"Dave", "Game1":"Indiana", "Game2":"Penn State"} 
-              ])
+key = pd.Series({"CFP_First_Round1": "2. Notre Dame"})
 
-results = df.iloc[:,1:] == st.session_state.key
-df["Wins"] = np.sum(results, axis=1)
+df["Wins"] = np.sum(df.eq(key), axis=1)
 
-data_window = st.dataframe(df[["Name", "Wins"]])
+df = df.sort_values("Wins", reversed=True)
 
-def callback_function1():
-    st.session_state.key["Game1"] = "Notre Dame"
-
-def callback_function2():
-    st.session_state.key["Game1"] = "Indiana"
-
-
-st.button("Notre Dame Wins", on_click=callback_function1)
-st.button("Indiana Wins", on_click=callback_function2)
+st.dataframe(df[["Name", "Wins"]])
