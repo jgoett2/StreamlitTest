39cb076c2257254ef5f098ac3fd1087c9dcdb130
Jeffrey Goett <jgoett2@yahoo.com>
2024-12-11 08:45:31 -0500

    updated selection

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("CFP")
 
 df = pd.read_csv("data.csv")
 
-name_conversion = {"open":"open", "Indiana":"1. Indiana", "Notre Dame":"2. Notre Dame"}
+name_conversion = {None:"open", "Indiana":"1. Indiana", "Notre Dame":"2. Notre Dame"}
 
 if "key" not in st.session_state:
   st.session_state.key = pd.Series({})
@@ -22,7 +22,8 @@ def callback_function():
     #print(st.session_state.Game1)
     st.session_state.key["CFP_First_Round1"] = name_conversion[st.session_state.CFP1]
     
-
-st.radio("CFP First Round 2 3", ["Indiana", "Notre Dame", "open"], key="CFP1", index=2, horizontal=True, on_change=callback_function)
+for i in range(0,10):
+  st.segmented_control("CFP", ["Indiana", "Notre Dame"], key="CFP"+str(i),  
+           on_change=callback_function)
 
 
