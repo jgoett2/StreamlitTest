714cc44f42717c094fc7f19ea5f1edd3f6bf0284
Jeffrey Goett <jgoett2@yahoo.com>
2024-12-12 19:00:37 -0500

    update calculation of losses

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ st.title("CFP")
 df = pd.read_csv("data.csv")
 games = pd.read_csv("key.csv")
 
-name_conversion = {None:"open", "Indiana":"1. Indiana", "Notre Dame":"2. Notre Dame"}
+name_conversion = {None:"open"}
 
 if "key" not in st.session_state:
   temp = games.set_index(games["Game"])
   st.session_state.key = pd.concat([temp.loc[temp["Team1_Victory"],"Team1"], temp.loc[temp["Team2_Victory"], "Team2"]]) 
 
 df["Wins"] = np.sum(df.eq(st.session_state.key), axis=1)
-df["Losses"] = len(st.session_state.key) - df["Wins"]
+df["Losses"] = len(st.session_state.key[st.session_state.key != "open"]) - df["Wins"]
 df = df.sort_values("Wins", ascending=False)
 
 col1, col2 = st.columns(2)
@@ -25,12 +25,14 @@ with col1:
 
 def callback_function(game):
     #print(st.session_state.Game1)
-    st.session_state.key[game] = st.session_state[game]
+    st.session_state.key[game] = name_conversion[st.session_state[game]]
 
 with col2:
   for open_game in games[(games.Team1_Victory == False) & (games.Team2_Victory == False)].values:
     team1 = open_game[1].strip(" 0123456789.")
     team2 = open_game[3].strip(" 0123456789.")
+    name_conversion[team1] = open_game[1]
+    name_conversion[team2] = open_game[3]
     st.segmented_control(open_game[0], [team1, team2], key=open_game[0],  
            on_change=callback_function, kwargs={"game":open_game[0]})
 
